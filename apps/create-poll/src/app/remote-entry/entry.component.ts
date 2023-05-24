234ad4d49f49fa19cbf80cf9a3290835aad78b09
gkotecki <gabrielkotecki@gmail.com>
2023-05-24 14:55:51 -0300

    ADD: host and remote working

---
+++ apps/create-poll/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pollerson-create-poll-entry',
+  template: `<pollerson-nx-welcome></pollerson-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
